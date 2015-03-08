69b9c48c4b763c6e1b38a167cccf44ddd24cd429
Justin 'Doychi' Deutsch <doychi@gmail.com>
2015-03-09 05:35:27 +1100

    Fix: Added the missing fish conf it last commit

---
+++ common/fish/fish/config.fish
@@ -0,0 +1,5 @@
+# Set up the enviornment
+
+# Set the path
+set -U fish_user_paths $fish_user_paths /home/doychi/.local/bin /usr/local/games
+
---
+++ common/fish/fish/functions/kpcli-doychi.fish
@@ -0,0 +1,5 @@
+function kpcli-doychi
+	kpcli --kdb=/home/doychi/netdrives/windows/host/giantbug/keepass/doychi.kdbx
+
+
+end
